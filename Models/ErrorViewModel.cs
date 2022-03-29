72047763d287ab391e78d75756f37cc71bc81335
pperorin <zentoa@hotmail.co.th>
2022-03-30 01:05:30 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Software_Studio_Project.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
