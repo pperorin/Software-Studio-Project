a3178a007eadd08724310eee0a1417889d835e05
Noppawat Khamyot <noppawatum@gmail.com>
2022-04-22 21:22:25 +0700

    change tailwind to bootstap

--- client_front/src/App.js
+++ client_front/src/App.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { HomePage } from './pages';
 
 function App() {
--- client_front/src/components/PageLayout/NavigationBar/index.js
+++ client_front/src/components/PageLayout/NavigationBar/index.js
@@ -2,15 +2,14 @@ import { Link } from 'react-router-dom';
 
 const NavigationBar = () => {
     return (
-        <nav class="container flex justify-around py-8 mx-auto bg-white">
-            <div>
-                <h3 class="text-2xl font-medium text-blue-500">LOGO</h3>
-            </div>
-            <div class="space-x-8 text-gray-500">
-                <Link to="/">Home</Link>
-                <Link to="/">link1</Link>
-                <Link to="/">link2</Link>
-            </div>
+        <nav className="navbar navbar-light bg-light justify-content-between">
+            <a className="navbar-brand">Navbar</a>
+            <form className="form-inline">
+                <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
+                    Search
+                </button>
+            </form>
         </nav>
     );
 };
