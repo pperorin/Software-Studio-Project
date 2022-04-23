30bf09dc65eaf67de6904ee5d3e563e070c42b6c
Noppawat Khamyot <noppawatum@gmail.com>
2022-04-24 00:43:11 +0700

    add threadpage , announcementpage

--- client_front/src/pages/AdminPage/components/MembersPage.js
+++ client_front/src/pages/AdminPage/components/MembersPage.js
@@ -2,10 +2,10 @@ const MembersPage = () => {
     return (
         <div className="container">
             <h2 className="mb-4">Member List</h2>
-            <ul class="list-group">
-                <li class="list-group-item d-flex">
-                    <p class="p-0 m-2 flex-grow-1">ชื่อ นามสกุล</p>
-                    <button class="btn-warning m-2">BAN</button> <button class="btn-danger m-2">DELETE</button>
+            <ul className="list-group">
+                <li className="list-group-item d-flex">
+                    <p className="p-0 m-2 flex-grow-1">ชื่อ นามสกุล</p>
+                    <button className="btn-warning m-2">BAN</button> <button className="btn-danger m-2">DELETE</button>
                 </li>
             </ul>
         </div>
