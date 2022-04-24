75d99b3d5900febdad42250086b4021f94f16e58
tchtan <tctan.ch@gmail.com>
2022-04-24 19:56:18 +0700

    announce

--- client_front/src/App.jsx
+++ client_front/src/App.jsx
@@ -1,12 +1,11 @@
 import { Navigate, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { HomePage, ProfilePage, SubjectPage, AdminPage } from './pages';
+import { HomePage, ProfilePage, SubjectPage, AdminPage, AnnouncementPage } from './pages';
 import LoginPage from './pages/LoginPage/LoginPage';
 import RegisterPage from './pages/RegisterPage/RegisterPage';
 
 import { useDispatch } from 'react-redux';
 import { userLogout } from './state/auth';
-import AnnouncementPage from './pages/AdminPage/components/AnnouncementPage';
 
 const Logout = () => {
     const dispatch = useDispatch();
@@ -20,11 +19,10 @@ function App() {
             <Route path="/" element={<HomePage />} />
             <Route path="subject" element={<SubjectPage />} />
             <Route path="/profile" element={<ProfilePage />} />
-            
+            <Route path="/Announcement" element={<AnnouncementPage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/logout" element={<Logout />} />
             <Route path="/register" element={<RegisterPage />} />
-
             <Route path="/admin/*" element={<AdminPage />} />
         </Routes>
     );
