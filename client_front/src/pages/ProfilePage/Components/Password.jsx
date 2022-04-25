904ea38171b8266f140b1c2d3830855899d0834d
Noppawat Khamyot <noppawatum@gmail.com>
2022-04-26 01:59:00 +0700

    add change password and logout

--- client_front/src/pages/ProfilePage/Components/Password.jsx
+++ client_front/src/pages/ProfilePage/Components/Password.jsx
@@ -1,31 +1,86 @@
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
-import { Form, Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import { Form, Button } from 'react-bootstrap';
 
-const Password = (props) => {
+const Password = () => {
+    const navigate = useNavigate();
+    const user = useSelector((state) => state.auth);
+    const [password, setPassword] = useState('');
+    const [newPassword, setNewPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    const onLogoutHandler = () => {
+        navigate('/logout');
+    };
+
+    const onClickChangePassword = async (e) => {
+        e.preventDefault();
+        if (newPassword === confirmPassword) {
+            try {
+                await axios.put(`https://localhost:7061/api/users/editPassword/${user.user.id}`, {
+                    password,
+                    NewPassword: newPassword,
+                });
+                alert('Password changed successfully');
+                setPassword('');
+                setNewPassword('');
+                setConfirmPassword('');
+            } catch (error) {
+                alert(error.message);
+            }
+        } else {
+            alert('Password not match');
+        }
+    };
+
     return (
         <div>
-            <Form>
+            <Form onSubmit={onClickChangePassword}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Current Password</Form.Label>
-                    <Form.Control type="email" placeholder="Enter Current Password" />
+                    <Form.Control
+                        type="password"
+                        placeholder="Enter Current Password"
+                        required
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password}
+                    />
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Group className="mb-3" controlId="newPassword">
                     <Form.Label>New Password</Form.Label>
-                    <Form.Control type="password" placeholder="Enter New Password" />
+                    <Form.Control
+                        type="password"
+                        placeholder="Enter New Password"
+                        required
+                        onChange={(e) => setNewPassword(e.target.value)}
+                        value={newPassword}
+                    />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Group className="mb-3" controlId="confirmNewPassword">
                     <Form.Label>Confirm Password</Form.Label>
-                    <Form.Control type="password" placeholder="Enter Confirm Password" />
+                    <Form.Control
+                        type="password"
+                        placeholder="Enter Confirm Password"
+                        required
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                        value={confirmPassword}
+                    />
                 </Form.Group>
                 <Button variant="primary float-end" type="submit">
                     Change Password
                 </Button>
+                <br></br>
+                <br></br>
+                <Button variant="danger float-end" onClick={onLogoutHandler}>
+                    Logout
+                </Button>
             </Form>
         </div>
     );
-}
+};
 
 export default Password;
--- client_front/src/pages/ProfilePage/ProfilePage.jsx
+++ client_front/src/pages/ProfilePage/ProfilePage.jsx
@@ -8,19 +8,23 @@ const ProfilePage = () => {
 
     return (
         <PageLayout>
-            <div className='center container'>
+            <div className="center container">
                 <div className="row">
                     <div className="col-2">
-                        <img src="https://www.nicepng.com/png/detail/128-1280406_view-user-icon-png-user-circle-icon-png.png" alt="profile" className='profileImg' />
+                        <img
+                            src="https://www.nicepng.com/png/detail/128-1280406_view-user-icon-png-user-circle-icon-png.png"
+                            alt="profile"
+                            className="profileImg"
+                        />
                     </div>
-                    <div className="col-10">
+                    <div className="col-10 mx-auto" style={{ maxWidth: '600px' }}>
                         <label htmlFor="">Username </label>
-                        <input type="text" className='username' disabled placeholder={user.user.username}/>
+                        <input type="text" className="username" disabled placeholder={user.user.username} />
                         <Password />
                     </div>
                 </div>
             </div>
-        </PageLayout >
+        </PageLayout>
     );
 };
 
